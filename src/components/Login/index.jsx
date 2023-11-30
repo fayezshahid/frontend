bdbbcba7aad5209373d04f0fd64cc7f0b3ffde0b
Fayez Shahid <fayezshahid167@gmail.com>
2023-12-01 00:51:25 +0500

    changed route url to test backend

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -14,7 +14,8 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/login";
+			// const url = "http://localhost:8080/api/login";
+			const url = "https://rgates10412839-prod.azurewebsites.net/api/login";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/";
